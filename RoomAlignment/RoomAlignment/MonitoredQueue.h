055c48989c8f86976b4f6b8819bce1c54e717e8f
Julian Löhr <julian.loehr@outlook.com>
2017-08-18 12:58:08 +0200

    Add template for Monitored Queue

---
+++ RoomAlignment/RoomAlignment/MonitoredQueue.h
@@ -0,0 +1,50 @@
+/*
+
+Copyright(C) 2017 Julian Löhr
+All rights reserved.
+
+Licensed under:
+MIT License
+
+*/
+#pragma once
+
+template<typename T>
+class MonitoredQueue {
+public:
+	typedef std::vector<T> QueueType;
+
+	MonitoredQueue()
+		:Empty(true) {}
+
+	bool IsEmpty() const 
+	{ 
+		return Empty; 
+	}
+
+	void Enqueue(const T && Data)
+	{
+		std::lock_guard<std::mutex> lock(Monitor);
+		Queue.push_back(Data);
+		Empty = false;
+		WakeDequeue.notify_one();
+	}
+
+	QueueType && Dequeue()
+	{
+		std::lock_guard<std::mutex> lock(Monitor);
+		if (IsEmpty())
+		{
+			WakeDequeue.wait(Monitor);
+		}
+
+		return std::move(Queue);
+	}
+
+private:
+	std::atomic_bool Empty;
+	std::vector<T> Queue;
+
+	std::mutex Monitor;
+	std::condition_variable_any WakeDequeue;
+};
