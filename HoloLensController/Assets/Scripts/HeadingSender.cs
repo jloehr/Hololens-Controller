7ba19379ca24d5a7dbad193a68ac28852114b1ca
Julian Löhr <julian.loehr@outlook.com>
2017-10-04 17:42:00 +0200

    Send Angle and Offset

--- HoloLensController/Assets/Scripts/HeadingSender.cs
+++ HoloLensController/Assets/Scripts/HeadingSender.cs
@@ -1,4 +1,5 @@
 ﻿using MQTT;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -8,13 +9,26 @@ public class HeadingSender : MonoBehaviour {
     public string TangoResetTopic;
     public string HoloLensHeadingTopic;
 
+    [Serializable]
+    private struct Message {
+        public float Angle;
+        public float X;
+        public float Y;
+        public float Z;
+    }
+
     void Start () {
         MQTTManager.Instance.Subscribe(TangoResetTopic, OnTangoReset);
 	}
 
     void OnTangoReset(string Topic, byte[] Data)
     {
-        Matrix4x4 Heading = transform.localToWorldMatrix;
+        Message Heading;
+        Heading.Angle = transform.rotation.eulerAngles.y;
+        Heading.X = transform.position.x;
+        Heading.Y = transform.position.y;
+        Heading.Z = transform.position.z;
+
         string AsString = JsonUtility.ToJson(Heading);
         byte[] AsByte = Encoding.UTF8.GetBytes(AsString);
         
