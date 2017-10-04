e9e0639745d964edf198c8736dc9dbe728a579f2
Julian Löhr <julian.loehr@outlook.com>
2017-10-04 13:26:49 +0200

    Add MQTT Recorder

---
+++ RoomAlignment/MQTTRecorder/stdafx.cpp
@@ -0,0 +1,10 @@
+/*
+
+Copyright(C) 2017 Julian Löhr
+All rights reserved.
+
+Licensed under:
+MIT License
+
+*/
+#include "stdafx.h"
---
+++ RoomAlignment/MQTTRecorder/targetver.h
@@ -0,0 +1,12 @@
+/*
+
+Copyright(C) 2017 Julian Löhr
+All rights reserved.
+
+Licensed under:
+MIT License
+
+*/
+#pragma once
+
+#include <SDKDDKVer.h>
